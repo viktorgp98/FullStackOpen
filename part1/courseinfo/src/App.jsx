3093cbeb55d5e16892eb64f000f10d362a66d694
viktorgp98 <97058781+viktorgp98@users.noreply.github.com>
2024-07-01 22:30:54 -0400

    upload part 1 exercise 1.1
    excercise 1.1 solution

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = (course) => {
+  return <h1>{course.course}</h1>;
+};
+
+const Part = (content) => {
+  return (
+    <p>
+      {content.part}. Exercises: {content.exercise}
+    </p>
+  );
+};
+
+const Content = (content) => {
+  return (
+    <div>
+      <Part
+        part={content.parts[0].name}
+        exercise={content.parts[0].exercises}
+      />
+      <Part
+        part={content.parts[1].name}
+        exercise={content.parts[1].exercises}
+      />
+      <Part
+        part={content.parts[2].name}
+        exercise={content.parts[2].exercises}
+      />
+    </div>
+  );
+};
+const Total = (total) => {
+  return (
+    <p>
+      Number of exercises:{" "}
+      {total.parts[0].exercises +
+        total.parts[1].exercises +
+        total.parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
